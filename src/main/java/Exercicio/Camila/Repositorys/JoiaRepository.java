a720fd7a59ee4946b13ffb3c534bfe7fc73f7d5b
Bruno Silva Barbosa <brunobarbosa@dgtbr.com.br>
2023-08-02 17:36:45 -0400

    Versão otimizada

--- src/main/java/Exercicio/Camila/Repositorys/JoiaRepository.java
+++ src/main/java/Exercicio/Camila/Repositorys/JoiaRepository.java
@@ -2,15 +2,12 @@ package Exercicio.Camila.Repositorys;
 
 import java.util.List;
 
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import Exercicio.Camila.Models.Joia;
 
 @Repository
 public interface JoiaRepository extends JpaRepository<Joia, Long> {
-    @Query("SELECT j FROM Joia j WHERE j.tipo = :tipo")
-    List<Joia> findAllByTipo(@Param("tipo") String tipo);
+    List<Joia> findAllByTipo(String tipo);
 }
